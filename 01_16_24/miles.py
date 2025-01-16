a13edf2b439a798502c5000337a400db5fe7da24
Angela Venable <avenable11@ivytech.edu>
2025-01-16 16:41:22 -0500

    string formatting and strings

--- 01_16_24/miles.py
+++ 01_16_24/miles.py
@@ -3,5 +3,6 @@ ending_mileage = int(input("Enter the ending mileage: "))
 gallons_used = int(input("Enter the gallons of gas used: "))
 distance_driven = ending_mileage - starting_mileage
 miles_per_gallon = distance_driven / gallons_used
-
-print("The miles per gallon is ", miles_per_gallon)
+starting_mileage = int(input("Enter the starting mileage: "))
+print(f"The mile's per gallon is {miles_per_gallon:.2f}")
+print('In the resources module on a page titled, "Viewing Lecture Recordings"')
