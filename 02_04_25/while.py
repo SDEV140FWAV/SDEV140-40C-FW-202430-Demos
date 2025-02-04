5c8599d8ef9ac177683b0cf042517b47673efe86
Angela Venable <avenable11@ivytech.edu>
2025-02-04 15:45:56 -0500

    while loops

--- 02_04_25/while.py
+++ 02_04_25/while.py
@@ -1,12 +1,19 @@
 """Author: Angela Venable"""
 
 theSum = 0.0
-data = input("Enter a number or just enter to quit: ")
-dataCpy = data.replace(".", "").replace("-","")
+data = " "
 
-while data != "" and dataCpy.isnumeric(): # and data.isdigit()
-    number = float(data)
-    theSum += number # theSum = theSum + number
+
+while data != "": # and data.isdigit()
     data = input("Enter a number or just enter to quit: ")
     dataCpy = data.replace(".", "").replace("-","")
+    
+    if not dataCpy.isnumeric() or data.count(".") > 1 or data.count("-") > 1:
+        if data != "":
+            print("The input is not a floating point number.")
+        continue
+    number = float(data)
+    theSum += number # theSum = theSum + number
+    
+    
 print(f"The sum is {theSum:.3f}")
