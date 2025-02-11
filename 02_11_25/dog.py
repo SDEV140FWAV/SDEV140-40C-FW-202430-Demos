3b9db124b0176f223675300a2ec803b7dca48630
Angela Venable <avenable11@ivytech.edu>
2025-02-11 17:04:45 -0500

    rational number class

--- 02_11_25/dog.py
+++ 02_11_25/dog.py
@@ -8,10 +8,22 @@ class Dog:
     def sit(self):
         """Simulate a dog sitting in response to a command"""
         print(f"{self.name} is now sitting.")
+    def roll_over(self):
+        """simulate rolling over in response to a command"""
+        print(f"{self.name} rolled over!")
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+    def set_age(self,age):
+        if self.age < age:
+            self.age = age
     def __str__(self):
         """create a string representation of a dog"""
         return f"Name: {self.name}\nAge: {self.age}"
 
 d = Dog("Molly",10)
 d.sit()
+print(d)
+d.name = "Suzy"
 print(d)
